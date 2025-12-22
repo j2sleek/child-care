8735997093c8c92b26d5f6546cf4c4698afd8dce
Teite Stephen Sampson <j2sleek@gmail.com>
2025-12-22 21:25:10 +0100

    added src/modules/children/child.model.ts

---
+++ backend/src/modules/children/child.model.ts
@@ -0,0 +1,30 @@
+import mongoose, { Schema, Document, Model, Types } from 'mongoose';
+
+export interface ChildDoc extends Document {
+  name: string;
+  dateOfBirth: Date;
+  createdBy: Types.ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const ChildSchema = new Schema<ChildDoc>({
+    name: { 
+      type: String, 
+      required: true 
+    },
+    dateOfBirth: { 
+      type: Date, 
+      required: true 
+    },
+    createdBy: { 
+      type: Schema.Types.ObjectId, 
+      ref: 'User', 
+      required: true 
+    }
+  },
+  { timestamps: true }
+);
+
+const ChildModel: Model<ChildDoc> = mongoose.model<ChildDoc>('Child', ChildSchema);
+export default ChildModel;
